c31a65c77fd4c3fbd5a347cb2ecffacbc572c3e4
Kadir Uludağ <kadiruludag@kadir-macbook-pro.local>
2018-11-12 11:28:34 +0300

    Initial Commit

---
+++ ObjectWithCode/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ObjectWithCode
+//
+//  Created by Kadir Uludağ on 12.11.2018.
+//  Copyright © 2018 Kadir Uludağ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
